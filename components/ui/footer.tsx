6ca2bcb7f81e0e5378dfeb9830b4d1f79a105fb8
Fellipe Marcel <fmrclms@gmail.com>
2023-11-13 16:42:52 -0300

    feat: add footer component and update home layout

---
+++ components/ui/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="bg-accent px-8 py-5 text-[0.625rem] opacity-75 text-center">
+      © 2023 Copyright - <span className="font-semibold">WondrCart</span>{" "}
+    </footer>
+  );
+};
+
+export default Footer;
