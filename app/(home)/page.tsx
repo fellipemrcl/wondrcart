196d5573a30119dde54981820e966328a7f22007
Fellipe Marcel <fmrclms@gmail.com>
2023-11-13 15:47:26 -0300

    feat: add banner and offer section to home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -25,9 +25,20 @@ const Home = async () => {
       <div className="mt-8 px-5">
         <Categories />
       </div>
+      
       <div className="mt-8">
+        <p className="font-bold uppercase pl-5 mb-2">Ofertas</p>
         <ProductList products={deals} />
       </div>
+
+      <Image
+        src="/banner-home-02.png"
+        height={0}
+        width={0}
+        alt="Até 55% de desconto em mouses"
+        className="h-auto w-full px-5"
+        sizes="100vw"
+      />
     </div>
   );
 };
