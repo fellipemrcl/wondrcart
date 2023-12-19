186e57f72a6237c0fce2b5f18fcf9e098ed33f78
Fellipe Marcel <fmrclms@gmail.com>
2023-12-19 14:54:00 -0300

    refactor: Cart component to display CartItems with total price

--- components/ui/cart.tsx
+++ components/ui/cart.tsx
@@ -2,12 +2,14 @@ import { ShoppingCartIcon } from "lucide-react";
 import { Badge } from "./badge";
 import { useContext } from "react";
 import { CartContext } from "@/providers/cart";
+import CartItem from "./cartItem";
+import { calculateProductsTotalPrice } from "@/helpers/product";
 
 const Cart = () => {
   const { products } = useContext(CartContext);
 
   return (
-    <div>
+    <div className="flex flex-col gap-8">
       <Badge
         className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
         variant={"outline"}
@@ -16,9 +18,14 @@ const Cart = () => {
         Carrinho
       </Badge>
 
-      {products.map((product) => (
-        <div key={product.id}>{product.name}</div>
-      ))}
+      <div className="flex flex-col gap-5">
+        {products.map((product) => (
+          <CartItem
+            key={product.id}
+            product={calculateProductsTotalPrice(product as any) as any}
+          />
+        ))}
+      </div>
     </div>
   );
 };
