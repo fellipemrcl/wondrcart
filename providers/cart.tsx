60c70847c1a70561b8606596ad4c26c0d6c774e8
Fellipe Marcel <fmrclms@gmail.com>
2024-04-08 14:28:52 -0300

    refactor: cart provider to include total, subtotal, and totalDiscount properties

--- providers/cart.tsx
+++ providers/cart.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { ProductsWithTotalprice } from "@/helpers/product";
-import { ReactNode, createContext, useState } from "react";
+import { ReactNode, createContext, useMemo, useState } from "react";
 
 export interface CartProduct extends ProductsWithTotalprice {
   quantity: number;
@@ -12,6 +12,9 @@ interface ICartContext {
   cartTotalPrice: number;
   cartBasePrice: number;
   cartTotalDiscount: number;
+  total: number;
+  subtotal: number;
+  totalDiscount: number;
   addProductToCart: (product: CartProduct) => void;
   decreaseProductQuantity: (productId: string) => void;
   increaseProductQuantity: (productId: string) => void;
@@ -23,6 +26,9 @@ export const CartContext = createContext<ICartContext>({
   cartTotalPrice: 0,
   cartBasePrice: 0,
   cartTotalDiscount: 0,
+  total: 0,
+  subtotal: 0,
+  totalDiscount: 0,
   addProductToCart: () => {},
   decreaseProductQuantity: () => {},
   increaseProductQuantity: () => {},
@@ -31,6 +37,22 @@ export const CartContext = createContext<ICartContext>({
 
 const CartProvider = ({ children }: { children: ReactNode }) => {
   const [products, setProducts] = useState<CartProduct[]>([]);
+
+  const subtotal = useMemo(() => {
+    return products.reduce(
+      (acc, product) => acc + Number(product.basePrice),
+      0,
+    );
+  }, [products]);
+
+  const total = useMemo(() => {
+    return products.reduce(
+      (acc, product) => acc + Number(product.totalPrice),
+      0,
+    );
+  }, [products]);
+
+  const totalDiscount = subtotal - total;
 
   const addProductToCart = (product: CartProduct) => {
     const productCartIsALreadyOnCart = products.some(
@@ -59,16 +81,18 @@ const CartProvider = ({ children }: { children: ReactNode }) => {
 
   const decreaseProductQuantity = (productId: string) => {
     setProducts((prevProducts) =>
-      prevProducts.map((cartProduct) => {
-        if (cartProduct.id === productId) {
-          return {
-            ...cartProduct,
-            quantity: cartProduct.quantity - 1,
-          };
-        }
+      prevProducts
+        .map((cartProduct) => {
+          if (cartProduct.id === productId) {
+            return {
+              ...cartProduct,
+              quantity: cartProduct.quantity - 1,
+            };
+          }
 
-        return cartProduct;
-      }).filter((cartProduct) => cartProduct.quantity > 0),
+          return cartProduct;
+        })
+        .filter((cartProduct) => cartProduct.quantity > 0),
     );
   };
 
@@ -86,7 +110,7 @@ const CartProvider = ({ children }: { children: ReactNode }) => {
       }),
     );
   };
-  
+
   const removeProductFromCart = (productId: string) => {
     setProducts((prevProducts) =>
       prevProducts.filter((cartProduct) => cartProduct.id !== productId),
@@ -104,6 +128,9 @@ const CartProvider = ({ children }: { children: ReactNode }) => {
         cartTotalPrice: 0,
         cartBasePrice: 0,
         cartTotalDiscount: 0,
+        total,
+        subtotal,
+        totalDiscount,
       }}
     >
       {children}
