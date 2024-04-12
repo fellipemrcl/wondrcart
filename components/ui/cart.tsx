9cd4267d4fc3e19020ad91fc83d0aa1fed63f8bf
Fellipe Marcel <fmrclms@gmail.com>
2024-04-12 15:13:53 -0300

    refactor: cart component to include Stripe payment integration

--- components/ui/cart.tsx
+++ components/ui/cart.tsx
@@ -7,9 +7,18 @@ import { calculateProductsTotalPrice } from "@/helpers/product";
 import { Separator } from "./separator";
 import { ScrollArea } from "./scroll-area";
 import { Button } from "./button";
+import { createCheckout } from "@/actions/checkout";
+import { loadStripe } from "@stripe/stripe-js";
 
 const Cart = () => {
   const { products, subtotal, total, totalDiscount } = useContext(CartContext);
+
+  const handleFinishPurchase = async () => {
+    const checkout = await createCheckout(products);
+    const stripe = await loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+
+    stripe?.redirectToCheckout({ sessionId: checkout.id });
+  };
 
   return (
     <div className="flex h-full flex-col gap-8">
@@ -21,9 +30,9 @@ const Cart = () => {
         Carrinho
       </Badge>
 
-      <div className="flex h-full flex-col gap-5 max-h-full overflow-hidden">
+      <div className="flex h-full max-h-full flex-col gap-5 overflow-hidden">
         <ScrollArea className="h-full">
-          <div className="flex flex-col gap-8 h-full">
+          <div className="flex h-full flex-col gap-8">
             {products.length > 0 ? (
               products.map((product) => (
                 <CartItem
@@ -64,7 +73,12 @@ const Cart = () => {
             <p>R$ {total.toFixed(2)}</p>
           </div>
 
-          <Button className="uppercase font-bold mt-7">Finalizar compra</Button>
+          <Button
+            className="mt-7 font-bold uppercase"
+            onClick={handleFinishPurchase}
+          >
+            Finalizar compra
+          </Button>
         </div>
       )}
     </div>
