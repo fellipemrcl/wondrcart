02fb989892c6f0e53731e5c158d13c3a3b8ee3c0
Fellipe Marcel <fmrclms@gmail.com>
2024-04-12 15:13:27 -0300

    feat: Add experimental serverActions configuration to next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   images: {
     domains: ["fsw-store.s3.sa-east-1.amazonaws.com", "i.ibb.co"],
   },
+  experimental: {
+    serverActions: true,
+  },
 };
 
 module.exports = nextConfig;
